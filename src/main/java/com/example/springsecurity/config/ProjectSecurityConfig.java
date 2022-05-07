48691db84e4080ce793f83c41b4f75e61aa582ec
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2022-05-07 19:29:53 +0530

    Section 4

--- src/main/java/com/example/springsecurity/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/springsecurity/config/ProjectSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -77,6 +78,6 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+		return new BCryptPasswordEncoder();
 	}
 }
